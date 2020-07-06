4d586e77be86d5050a81c7d3a84f41b353602d49
Yudai Ozawa <yudai@YusMac.local>
2020-07-06 14:41:13 +0900

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,9 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+/* ここに作ったコンポーネントを記載する */
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('sample-component', require('./components/SampleComponent.vue').default);
 Vue.component('sample-component', require('./components/SampleComponent.vue').default);
 
 const app = new Vue({
