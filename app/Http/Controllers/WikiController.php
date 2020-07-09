11093c1661d2898487a3e2b0f380b339f1a0ecd1
堀田知生 <tomoki@cocacola.local>
2020-07-09 13:31:40 +0900

    [update] 返却値追加

--- app/Http/Controllers/WikiController.php
+++ app/Http/Controllers/WikiController.php
@@ -6,20 +6,10 @@ use Illuminate\Http\Request;
 use App\Wiki;
 use App\Wiki_log;
 use App\Chart_log;
+use Illuminate\Http\Response;
 
 class WikiController extends Controller
 {
-    // 全データ取得
-    public function wikiData()
-    {
-        $wiki_info = Wiki::all();
-        $value[0] = $wiki_info;
-        $wiki_log = Wiki_log::all();
-        $value[1] = $wiki_log;
-        $chart_log = Chart_log::all();
-        $value[2] = $chart_log;
-        return $value;
-    }
 
     // 店舗ごとのWiki取得
     public function select(){
@@ -30,8 +20,7 @@ class WikiController extends Controller
         $store_id = json_decode($store_info);
 
         $wiki = new Wiki();
-        $wiki_info = $wiki->wikiSelect($store_id->store_id);
-        return $wiki_info;
+        return $wiki->wiki_select($store_id->store_id);
     }
 
     // wikiの登録及びUPDATE
@@ -40,19 +29,29 @@ class WikiController extends Controller
         $request = new Request();
         $request->user_id = 1;
         $request->store_id = 1;
-        $request->text = 'bbb';
+        $request->text = 'ooo';
         $request->store_user_sum = 1;
         $json = json_encode($request);
         $register = json_decode($json);
 
         
         $wiki = new Wiki();
-        $wiki->wikiRegister($register);
+        if($wiki->wiki_register($register)){
+            $flag = response()->json([],200);
+        }else {
+            $flag = response()->json([],404);
+        }
 
         // ログの更新
-        $wiki_id = Wiki::all()->where('store_id', $register->store_id)->first()->id;
-        $wiki_log = new Wiki_log();
-        $wiki_log->wikiLogRegister($wiki_id, $register->user_id);
+        if($flag->status() == 200){
+            $wiki_id = Wiki::all()->where('store_id', $register->store_id)->first()->id;
+            $wiki_log = new Wiki_log();
+            $wiki_log->wiki_log_register($wiki_id, $register->user_id);
+            return $flag->status();
+        }else {
+            return $flag->status();
+        }
+
     }
 
     // Wikiの削除
@@ -65,7 +64,11 @@ class WikiController extends Controller
         $register = json_decode($json);
 
         $wiki = new Wiki();
-        $wiki->wikiDelete($register);
+        if($wiki->wiki_delete($register)){
+            return response()->json(['message'=>'aaa'],200);
+        }else {
+            return response()->json(['message'=>'bbb'],404);
+        }
     }
 
 }
