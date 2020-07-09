cf3f7360a418342eedc5eab6d25ac3213990c84e
堀田知生 <tomoki@cocacola.local>
2020-07-09 15:10:25 +0900

    first commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/wiki', 'ReviewController@review_wiki_select');
+
+Route::get('/', 'WikiController@wikiData');
