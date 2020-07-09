d08679d16074853fbd6c71cea2b269e8a96498e3
堀田知生 <tomoki@cocacola.local>
2020-07-09 13:33:40 +0900

    [add] model

--- app/Wiki.php
+++ app/Wiki.php
@@ -2,8 +2,10 @@
 
 namespace App;
 
+use Facade\FlareClient\Http\Response;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Http;
 
 class Wiki extends Model
 {
@@ -41,45 +43,52 @@ class Wiki extends Model
         return  $this->belongsTo('App\Store', 'store_id');
     }
 
-    public function wikiSelect($store_id)
+    public function wiki_select($store_id)
     {
-        $wiki_info = Wiki::all()->where('store_id', $store_id)->first();
-        return $wiki_info;
+        $wiki = Wiki::all()->where('store_id', $store_id)->first();
+        return $wiki;
     }
 
-    public function wikiRegister($register)
+    public function wiki_register($register)
     {
         // 削除済みか判断するために復元
         Wiki::onlyTrashed()->where('store_id', $register->store_id)->restore();
         // 行があるかの確認
-        $wiki_info = Wiki::all()->where('store_id', $register->store_id)->first();
-        if($wiki_info){
+        $wiki_flag = Wiki::all()->where('store_id', $register->store_id)->first();
+        if($wiki_flag){
             // あった場合はtextの更新
-            $wiki_info->text = $register->text;
-            $wiki_info->save();
+            $wiki_flag->text = $register->text;
+            $flag = $wiki_flag->save();
+            return $flag;
         }else {
             // ない場合はそのまま登録
             $wiki = new Wiki();
             $wiki->store_id = $register->store_id;
             $wiki->text = $register->text;
             $wiki->store_user_sum = $register->store_user_sum;
-            $wiki->save();
+            $flag = $wiki->save();
+            return $flag;
         }
+
+        return false;
     }
 
-    public function wikiDelete($register)
+    public function wiki_delete($register)
     {
         // テーブルのあるか確認
         $wiki = new Wiki();
-        $wiki_info = $wiki->wikiSelect($register->store_id);
-        if($wiki_info){
+        $wiki_flag = $wiki->wiki_select($register->store_id);
+        if($wiki_flag){
             // 更新
             $wiki_log = new Wiki_log();
-            $wiki_log->wikiLogRegister($wiki_info->id, $register->user_id);
+            $wiki_log->wiki_log_register($wiki_flag->id, $register->user_id);
 
             // あった場合は削除
-            $wiki_info->delete();
+            $wiki_flag->delete();
+            return true;
 
+        }else {
+            return false;
         }
     }
 }
