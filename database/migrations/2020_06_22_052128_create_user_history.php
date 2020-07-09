d34c00a71b7993edf47538c0d3db3573579adb31
堀田知生 <tomoki@cocacola.local>
2020-07-09 13:37:19 +0900

    [update] user->usersに変更

--- database/migrations/2020_06_22_052128_create_user_history.php
+++ database/migrations/2020_06_22_052128_create_user_history.php
@@ -17,7 +17,7 @@ class CreateUserHistory extends Migration
             $table->increments('id');
             $table->unsignedInteger('user_id');
             $table->unsignedInteger('store_id');
-            $table->foreign('user_id')->references('id')->on('user');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('store_id')->references('id')->on('store');
         });
     }
