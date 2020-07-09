e0b12f041648a9119b93b0950c31ea8cec714fbb
WataruTanaka0730 <watarutanaka0730@gmail.com>
2020-07-09 14:57:13 +0900

    [Update]にid追加

--- public/js/app.js
+++ public/js/app.js
@@ -49828,7 +49828,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nValidationError: Invalid options object. Sass Loader has been initialized using an options object that does not match the API schema.\n - options has an unknown property 'outputStyle'. These properties are valid:\n   object { implementation?, sassOptions?, prependData?, sourceMap?, webpackImporter? }\n    at validate (C:\\Users\\81808\\IT\\RamenWiki\\node_modules\\schema-utils\\dist\\validate.js:96:11)\n    at Object.loader (C:\\Users\\81808\\IT\\RamenWiki\\node_modules\\sass-loader\\dist\\index.js:36:28)\n    at C:\\Users\\81808\\IT\\RamenWiki\\node_modules\\webpack\\lib\\NormalModule.js:316:20\n    at C:\\Users\\81808\\IT\\RamenWiki\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\Users\\81808\\IT\\RamenWiki\\node_modules\\loader-runner\\lib\\LoaderRunner.js:233:18\n    at runSyncOrAsync (C:\\Users\\81808\\IT\\RamenWiki\\node_modules\\loader-runner\\lib\\LoaderRunner.js:143:3)\n    at iterateNormalLoaders (C:\\Users\\81808\\IT\\RamenWiki\\node_modules\\loader-runner\\lib\\LoaderRunner.js:232:2)\n    at C:\\Users\\81808\\IT\\RamenWiki\\node_modules\\loader-runner\\lib\\LoaderRunner.js:205:4\n    at C:\\Users\\81808\\IT\\RamenWiki\\node_modules\\enhanced-resolve\\lib\\CachedInputFileSystem.js:85:15\n    at processTicksAndRejections (internal/process/task_queues.js:76:11)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 
