4d29d75cd45a4413f37318a289fe57dde51f73f5
堀田知生 <tomoki@cocacola.local>
2020-07-13 13:22:06 +0900

    [update] 変数名を変更

--- app/Result_chart.php
+++ app/Result_chart.php
@@ -9,39 +9,44 @@ class Result_chart extends Model
     //
     protected $table = 'result_chart';
 
+    public function chart_log_result($wiki_id){
+        $result_chart = Result_chart::all()->where('wiki_id',$wiki_id)->first();
+        return $result_chart;
+    }
+
     public function chart_log_result_select($wiki_id){
         $chart_log = Chart_log::where('wiki_id',$wiki_id);
-        $resultChart = new Result_chart();
+        $result_chart = new Result_chart();
         $cnt = $chart_log->count();
-        $resultChart->sum_people_price = $chart_log->count();
-        $resultChart->avg_price = $chart_log->sum('price') / $cnt;
-        $resultChart->avg_amount = $chart_log->sum('amount') / $cnt;
-        $resultChart->avg_kotteri = $chart_log->sum('kotteri') / $cnt;
-        $resultChart->avg_soup = $chart_log->sum('soup') / $cnt;
-        $resultChart->avg_topping = $chart_log->sum('topping') / $cnt;
-        $resultChart->avg_noodle_type = $chart_log->sum('noodle_type') / $cnt;
-        $resultChart->wiki_id = $wiki_id;
-        return $resultChart;
+        $result_chart->sum_people_price = $chart_log->count();
+        $result_chart->avg_price = $chart_log->sum('price') / $cnt;
+        $result_chart->avg_amount = $chart_log->sum('amount') / $cnt;
+        $result_chart->avg_kotteri = $chart_log->sum('kotteri') / $cnt;
+        $result_chart->avg_soup = $chart_log->sum('soup') / $cnt;
+        $result_chart->avg_topping = $chart_log->sum('topping') / $cnt;
+        $result_chart->avg_noodle_type = $chart_log->sum('noodle_type') / $cnt;
+        $result_chart->wiki_id = $wiki_id;
+        return $result_chart;
     }
 
     public function chart_log_result_register($wiki_id){
         $chart = new Result_chart();
-        $resultFlag = Result_chart::all()->where('wiki_id',$wiki_id)->first();
-        if($resultFlag){
-            $resultchart = $chart->chart_log_result_select($wiki_id);
-            $resultFlag->avg_price = $resultchart->avg_price;
-            $resultFlag->avg_amount = $resultchart->avg_amount;
-            $resultFlag->avg_kotteri = $resultchart->avg_kotteri;
-            $resultFlag->avg_soup = $resultchart->avg_soup;
-            $resultFlag->avg_topping = $resultchart->avg_topping;
-            $resultFlag->avg_noodle_type = $resultchart->avg_noodle_type;
-            $resultFlag->sum_people_price = $resultchart->sum_people_price;
-            $resultFlag->save();
-            return $resultFlag->id;
+        $result_flag = Result_chart::all()->where('wiki_id',$wiki_id)->first();
+        if($result_flag){
+            $result_chart = $chart->chart_log_result_select($wiki_id);
+            $result_flag->avg_price = $result_chart->avg_price;
+            $result_flag->avg_amount = $result_chart->avg_amount;
+            $result_flag->avg_kotteri = $result_chart->avg_kotteri;
+            $result_flag->avg_soup = $result_chart->avg_soup;
+            $result_flag->avg_topping = $result_chart->avg_topping;
+            $result_flag->avg_noodle_type = $result_chart->avg_noodle_type;
+            $result_flag->sum_people_price = $result_chart->sum_people_price;
+            $result_flag->save();
+            return $result_flag->id;
         }else {
-            $resultchart = $chart->chart_log_result_select($wiki_id);
-            $resultchart->save();
-            return $resultchart->id;
+            $result_chart = $chart->chart_log_result_select($wiki_id);
+            $result_chart->save();
+            return $result_chart->id;
         }
     }
 }
