58d2e9d06ad044990d5617b40d224d674a1de14f
Cherry <ex00ia2432@gmail.com>
2020-07-16 20:18:53 +0900

    [fix] class

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <div class="container wrap">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
