3196a6710725bec378b739b49ac6e5b25ab0a8c2
kojikoji-5046 <koppi.1998.jub@icloud.com>
2020-07-20 12:55:38 +0900

    [add]追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,9 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('sample-component', require('./components/SampleComponent.vue').default);
+Vue.component('menubar-notlogin-component', require('./components/Menubar_NotLogin.vue').default);
+Vue.component('searchview-component', require('./components/SearchView.vue').default);
+
 
 const app = new Vue({
     el: '#app'
