11c60c29579ef75b99a9381bb4de9e0aefca8b5e
Cherry <ex00ia2432@gmail.com>
2020-07-20 21:18:07 +0900

    [update] FormComponent

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,15 @@
 <head>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" type="text/css">
     <meta name="csrf-token" content="{{csrf_token()}}">
+    <script>
+        window.Laravel = {
+            csrfToken: "{{ csrf_token() }}"
+        };
+    </script>
 </head>
 <div id="app" class="wrap">
     <example-component></example-component>
     <sample-component></sample-component>
+    <form-component></form-component>
 </div>
 <script src="{{ mix('/js/app.js')  }}"></script>
