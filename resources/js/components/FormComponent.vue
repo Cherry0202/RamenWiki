d1185dc7a310bcb510b20637704527d55b9fd9fb
Cherry <ex00ia2432@gmail.com>
2020-07-20 21:16:11 +0900

    [add]FormComponent

---
+++ resources/js/components/FormComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+    <input type="text" v-model="keyword" placeholder="Search...">
+    <button v-on:click="postKeyword">post</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "FormComponent",
+        data() {
+            return {
+                keyword: '',
+                lists: null
+            }
+        },
+        methods: {
+            postKeyword(){
+                axios.post('/search',this.keyword)
+                .then( response => {
+                    console.log(response.data);
+                    this.lists = response.data
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
+<!--    TODO: buttonなしで送信 -->
+<!--    TODO: listsをprops-->
+<!-- TODO:scss -->
