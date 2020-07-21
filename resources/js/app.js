19c9c4df653d1f3b489d8c963a90c5e09a659ad1
Cherry <ex00ia2432@gmail.com>
2020-07-21 20:11:43 +0900

    [update] search-view-component

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ window.Vue = require('vue');
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('sample-component', require('./components/SampleComponent.vue').default);
 Vue.component('menubar-not-login-component', require('./components/MenubarNotLogin').default);
+Vue.component('search-view-component', require('./components/SearchView').default);
 
 const app = new Vue({
     el: '#app'
