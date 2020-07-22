88176f733a93874502d6d82b72cb7a632b295dc6
Cherry <ex00ia2432@gmail.com>
2020-07-22 19:58:56 +0900

    [add]ResultCardComponent.Vue

---
+++ resources/js/components/ResultCardComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card-wrapper">
+        カードだよ
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ResultCardComponent",
+        data() {
+            return {
+                results: [],
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .card-wrapper {
+        width: 152px;
+        height: 172px;
+        background-color: whitesmoke;
+    }
+</style>
+
+<!--  TODO: photoref-->
