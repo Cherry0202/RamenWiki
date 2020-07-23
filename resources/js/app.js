9f5048cd4e019d99695c8259ab315efbdcd576e8
Cherry <ex00ia2432@gmail.com>
2020-07-23 12:07:22 +0900

    [update] component path

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 Vue.component('sample-component', require('./components/SampleComponent.vue').default);
 Vue.component('search-form-component', require('./components/SearchFormComponent').default);
 Vue.component('menubar-not-login-component', require('./components/MenubarNotLogin').default);
-Vue.component('search-view-component', require('./components/SearchView').default);
+Vue.component('search-view', require('./pages/SearchView').default);
 Vue.component('result-card-component', require('./components/ResultCardComponent').default);
 
 
