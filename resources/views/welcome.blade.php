75b22e2a1ed48ef28ad6be5e75257b59cf527fe3
morrisharuka <yako0073@gmail.com>
2020-07-23 11:27:46 +0900

    [update]

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,5 @@
     <meta name="csrf-token" content="{{csrf_token()}}">
 </head>
 <div id="app" class="wrap">
-    <example-component></example-component>
-    <sample-component></sample-component>
 </div>
 <script src="{{ mix('/js/app.js')  }}"></script>
