ce3b8d635b9a56ceb2a9588205ce1600c835d3a0
Cherry <ex00ia2432@gmail.com>
2020-07-25 20:15:25 +0900

    [update]pages

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,16 @@ Vue.component('sample-component', require('./components/SampleComponent.vue').de
 Vue.component('search-form-component', require('./components/SearchFormComponent').default);
 Vue.component('menubar-not-login-component', require('./components/MenubarNotLogin').default);
 Vue.component('result-card-component', require('./components/ResultCardComponent').default);
+//pages
 Vue.component('search-view', require('./pages/SearchView').default);
+Vue.component('store-detail', require('./pages/StoreDetail').default);
+Vue.component('delete-account', require('./pages/DeleteAccount').default);
+Vue.component('my-page', require('./pages/MyPage').default);
+Vue.component('recommend-ramen', require('./pages/RecommendRamen').default);
+Vue.component('review-detail', require('./pages/ReviewDetail').default);
+Vue.component('review-list', require('./pages/ReviewList').default);
+Vue.component('create-view', require('./pages/CreateReview').default);
+
 
 const app = new Vue({
     el: '#app'
