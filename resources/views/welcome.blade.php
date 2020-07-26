269c5b0adebd7ef05456e3cfa437779fdbe3b660
kojikoji-5046 <koppi.1998.jub@icloud.com>
2020-07-26 23:59:43 +0900

    [add]追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,17 @@
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" type="text/css">
     <meta name="csrf-token" content="{{csrf_token()}}">
 </head>
-<div id="app" class="wrap">
-    <example-component></example-component>
-    <sample-component></sample-component>
+<div id="app" v-bind:class="{ active: ActiveBtn}">
+    <!--ハンバーガーメニュー（ログイン済み）-->
+    <humberger-logged-component></humberger-logged-component>
+    
+    <!--ハンバーガーメニュー（未ログイン）-->
+    <!--<humberger-menu-component></humberger-menu-component>-->
+
+    <!--検索画面（TOP)-->
+    <!--<searchview-component></searchview-component>-->
 </div>
 <script src="{{ mix('/js/app.js')  }}"></script>
