0326217e69740ccf79b3fe83ce81ba1c5a7a92bd
kojikoji-5046 <koppi.1998.jub@icloud.com>
2020-07-26 23:54:28 +0900

    [add]追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,9 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('sample-component', require('./components/SampleComponent.vue').default);
-Vue.component('menubar-notlogin-component', require('./components/Menubar_NotLogin.vue').default);
-Vue.component('searchview-component', require('./components/SearchView.vue').default);
-Vue.component('humberger-menu-component', require('./components/Hamburger_Menu.vue').default);
+Vue.component('searchview-component', require('./components/SearchView.vue').default);//検索画面（TOP）--topの分析チャート・店舗取得未完
+Vue.component('humberger-menu-component', require('./components/Hamburger_Menu.vue').default);//ハンバーガーメニュー未ログイン
+Vue.component('humberger-logged-component', require('./components/Hamburger_Loggedin.vue').default);//ハンバーガーメニュー（ログイン済み）
 
 
 const app = new Vue({
