a801ea98470e8bdbca29c6a5010968f98eb887b1
kojikoji-5046 <koppi.1998.jub@icloud.com>
2020-07-26 15:14:23 +0900

    [add]追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,14 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 Vue.component('sample-component', require('./components/SampleComponent.vue').default);
 Vue.component('menubar-notlogin-component', require('./components/Menubar_NotLogin.vue').default);
 Vue.component('searchview-component', require('./components/SearchView.vue').default);
+Vue.component('humberger-menu-component', require('./components/Hamburger_Menu.vue').default);
 
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: function() {
+        return{
+            ActiveBtn : false,
+       }
+    }
 });
