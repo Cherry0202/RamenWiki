986ba67862d9cb1f978eb09bb7871d486d951bae
morrisharuka <yako0073@gmail.com>
2020-07-27 12:18:27 +0900

    hoge

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,8 @@
     <meta name="csrf-token" content="{{csrf_token()}}">
 </head>
 <div id="app" class="wrap">
+    Email:<input type="text" name="Email" size="20">
+    Password:<input type="password" name="password" size="20">
+    <input type="submit" name="CONTINUE" value="CONTINUE" size="10" style="background-color:#5b8bd0">
 </div>
 <script src="{{ mix('/js/app.js')  }}"></script>
