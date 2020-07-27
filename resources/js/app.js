8a750d5a76f254712f52285e215b05d8a88b0d86
Yudai Ozawa <yudai@YusMac.local>
2020-07-27 15:17:17 +0900

    aaa

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,7 @@ Vue.component('wiki-edit', require('./components/Wikiedit.vue').default);
 Vue.component('mypage-passchange', require('./components/Mypage_Passchange.vue').default);
 Vue.component('ai-page', require('./components/AIPage.vue').default);
 Vue.component('goodbye-page', require('./components/Goodbye.vue').default);
-Vue.component('goodbyebye-page', require('./components/Goodbye_bye.vue').default);
+Vue.component('goodbyebye-page', require('./components/Goodbye_bye.vue').default)
 Vue.component(
     "login-component",
     require("./components/LoginComponent").default
