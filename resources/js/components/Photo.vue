e51b64d7c21abd258ba6cde7219aefb342a466ae
堀田知生 <tomoki@cocacola.local>
2020-07-28 10:56:48 +0900

    [add] mypageのコンポーネント

---
+++ resources/js/components/Photo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="imgBox">
+    <img
+      :src="item.url"
+      >  
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
