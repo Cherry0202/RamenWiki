426aaf544961b6c651dcdb6b058b47efd10b9f47
堀田知生 <tomoki@cocacola.local>
2020-07-28 10:53:31 +0900

    [update] mypageのblade

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -1,8 +1,10 @@
 <head>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ mix('/css/mypage.css') }}" rel="stylesheet" type="text/css">
     <meta name="csrf-token" content="{{csrf_token()}}">
 </head>
-    <div id="app">
-        <mypage-component></mypage-component>
+    <div id="app" class="wrap">
+      <mypage></mypage>
     </div>
 <script src="{{ mix('/js/app.js')  }}"></script>
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
