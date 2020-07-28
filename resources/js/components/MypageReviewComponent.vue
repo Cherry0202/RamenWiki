ac84bed57d96b646347fb7cddbebc40fd64a8fbc
堀田知生 <tomoki@cocacola.local>
2020-07-28 13:32:47 +0900

    [update] データが空の場合の表示を追加

--- resources/js/components/MypageReviewComponent.vue
+++ resources/js/components/MypageReviewComponent.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="reviewlist">
-    <Review
+    <div v-if="!reviews.length" class="no-review">
+      <p>まだレビューを書いていません！！</p>
+      <p>新しくレビューを作成して<br>みんなにお店のいいところを伝えよう！</p>
+      <img :src="noReviewSrc" alt="">
+    </div>
+    <div v-else>
+      <Review
         v-for="review in reviews"
         :key="review.id"
         :item="review"
-    />  
+      />
+    </div>
     </div>
 </template>
 
@@ -12,7 +19,9 @@
 export default {
   data() {
     return {
-      reviews: [{"store_name":"ラーメン江畑","text":"hogehogehoge","url":"/storage/review/review.jpeg"},{"store_name":"ラーメン江畑","text":"hogehogehoge","url":"/storage/review/review2.jpeg"}]
+      reviews: [],
+      // reviews: [{"store_name":"ラーメン江畑","text":"hogehogehoge","url":"/storage/review/review.jpeg"},{"store_name":"ラーメン江畑","text":"hogehogehoge","url":"/storage/review/review2.jpeg"}],
+      noReviewSrc: './images/aisatsu_kodomo_boy.png'
     }
   }
 
