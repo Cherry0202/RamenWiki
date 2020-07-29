aea37984d57014dbba8d413292a39f10a0cd1be8
Cherry <ex00ia2432@gmail.com>
2020-07-29 22:12:23 +0900

    [update]SearchView and 2 gmap

--- resources/js/pages/SearchView2.vue
+++ resources/js/pages/SearchView2.vue
@@ -139,7 +139,7 @@
                 top: 0;
                 height: 100vh;
                 z-index: 1;
-                background-image: url("../Ozawa_files/sample-map2.png");
+                background-image: url("../images/map-ss.png");
                 background-repeat: no-repeat;
                 background-size: cover;
             }
