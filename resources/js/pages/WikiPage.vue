082a85332b2be25a6066465ac2079486acf317e5
Cherry <ex00ia2432@gmail.com>
2020-07-29 17:26:49 +0900

    [update]WikiPage

--- resources/js/pages/WikiPage.vue
+++ resources/js/pages/WikiPage.vue
@@ -1,10 +1,11 @@
 <template>
-    <div class="wrap">
-    <form method="post" action="#">
+    <div class="wraps">
+        <form method="post" action="#">
             <div class="main_photo">
                 <img alt="photo" src="../Ozawa_files/Sample_ramenshot.jpg">
-                <div class="grad"></div>
-                <p class="name">{{ store_name }}</p>
+                <div class="grad">
+                    <p class="name">{{store_name}}</p>
+                </div>
                 <p class="yasi">⬅</p>
             </div>
             <div class="main_line">
@@ -12,58 +13,55 @@
             </div>
             <div class="main_text">
                 <p class="Midashi">店舗詳細</p>
-<!--                <input type="button" class="edit" value="　Edit　">-->
-                <router-link :to="{ name: 'edit' }" class="edit">
-                    Edit
-                </router-link>
+                <input type="button" class="edit" value="　Edit　">
                 <ui class="icon">
                     <li><img src="../Ozawa_files/icon1.png"></li>
                     <li><img src="../Ozawa_files/icon2.jpg"></li>
                     <li><img src="../Ozawa_files/icon3.png"></li>
                 </ui>
                 <!----------------------------------------------------------------------------------------------------------------------------------------->
+
                 <div class="Honbun">
                     <p>
-                        京都鶏ガラとんこつ醤油ラーメン屋台味(半熟味玉付・820円＋税)がおすすめ
-                    <br>
-                        味玉がセットだと無料
-                    <br>
-                    hogeeeeeeeee
+                        hogehoge
+                        <br>
+                        hoge
+                        <br>
+                        hogeeeeeeeee
                     </p>
                 </div>
 
                 <!----------------------------------------------------------------------------------------------------------------------------------------->
-                <hr>
+                <hr class="hhhh">
                 <!----------------------------------------------------------------------------------------------------------------------------------------->
 
                 <div class="chart">
                     <img src="../Ozawa_files/chart.png">
-<!--                    <h1>投稿者{{ user_sum }}人の平均</h1>-->
                 </div>
 
                 <!----------------------------------------------------------------------------------------------------------------------------------------->
                 <!----------------------------------------------------------------------------------------------------------------------------------------->
 
-                <div class="second_text">
-                    <hr>
-                    <div class="phone_g">
+                <hr class="hoge">
+                <div class="phone_g">
+                    <div class="aaaaaaaaaaaaaaa">
                         <img class="phone" src="../Ozawa_files/phone.png">
                         <p>
                             {{phone_number}}
                         </p>
                     </div>
-                    <hr>
-                    <!--  Map  -->
-<!--                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.4880141904177!2d139.6983826156727!3d35.68960668019239!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188cd0d6b1ba1f%3A0x1c32a1f1ecacfdd5!2z5paw5a6_6aeF!5e0!3m2!1sja!2sjp!4v1594865322873!5m2!1sja!2sjp" width="300" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>-->
-                    <iframe src="https://maps.google.co.jp/maps?output=embed&q=よってこや 新宿南口店" width="300" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-                    <hr>
+                </div>
+                <hr class="hoge hoge2">
+                <!--  Map  -->
+                <iframe src="https://maps.google.co.jp/maps?output=embed&q=よってこや 新宿南口店" width="300" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                <hr class="hoge">
 
-                    <p class="Sub_Midashi">最新レビュー</p>
-                    <div class="accbox">
+                <p class="Sub_Midashi">最新レビュー</p>
+                <div class="accbox">
                     <!----------------------------------------------------------------------------------------------------------------------------------------->
-                        <label for="label1">Username1</label>
-                        <input type="checkbox" id="label1" class="cssacc" />
-                        <div class="accshow">
+                    <label for="label1">Username1</label>
+                    <input type="checkbox" id="label1" class="cssacc" />
+                    <div class="accshow">
                         <!--body-->
                         <p>
                             RRRRRR
@@ -71,65 +69,64 @@
                             RRRRRRRRRRRRRRRRRRRRRR
                             <br>
                         </p>
-                        </div>
+                    </div>
                     <!----------------------------------------------------------------------------------------------------------------------------------------->
-                        <label for="label2">Username2</label>
-                        <input type="checkbox" id="label2" class="cssacc" />
-                        <div class="accshow">
+                    <label for="label2">Username2</label>
+                    <input type="checkbox" id="label2" class="cssacc" />
+                    <div class="accshow">
                         <!--body-->
                         <p>
                             hoge
                         </p>
-                        </div>
+                    </div>
                     <!----------------------------------------------------------------------------------------------------------------------------------------->
-                    </div>
+                </div>
 
-                    <hr>
+                <hr class="hoge">
 
-                    <div class="Buttons">
-                        <a href="#" class="btn1">レビューを書く</a>
-                        <a href="#" class="btn2">すべてのレビューを見る</a>
-                        <a href="#" class="btn3">この店舗に行ったことがある</a>
-                        <a href="https://www.google.co.jp/maps/dir/%E6%9D%B1%E4%BA%AC%E9%83%BD%E6%96%B0%E5%AE%BF%E5%8C%BA%E8%A5%BF%E6%96%B0%E5%AE%BF%EF%BC%91%E4%B8%81%E7%9B%AE%EF%BC%97+%E7%B7%8F%E5%90%88%E6%A0%A1%E8%88%8E%E3%82%B3%E3%82%AF%E3%83%BC%E3%83%B3%E3%82%BF%E3%83%AF+HAL%E6%9D%B1%E4%BA%AC/%E6%9D%B1%E4%BA%AC%E9%83%BD%E6%96%B0%E5%AE%BF%E5%8C%BA%E6%96%B0%E5%AE%BF%EF%BC%93%E4%B8%81%E7%9B%AE%EF%BC%93%EF%BC%95%E2%88%92%EF%BC%91%EF%BC%93+%E3%82%88%E3%81%A3%E3%81%A6%E3%81%93%E3%82%84+%E6%96%B0%E5%AE%BF%E5%8D%97%E5%8F%A3%E5%BA%97/@35.689994,139.6979404,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x60188cd6981dce11:0xee411b3fa3fc9f13!2m2!1d139.6969368!2d35.6916554!1m5!1m1!1s0x60188cdaf48693a1:0x4d4ff726e7fb4a72!2m2!1d139.7026238!2d35.6901278!3e0" class="btn4">GoogleMapでルート案内する</a>
-                    </div>
+                <div class="Buttons">
+                    <a href="#" class="btn1">レビューを書く</a>
+                    <a href="#" class="btn2">すべてのレビューを見る</a>
+                    <a href="#" class="btn3">この店舗に行ったことがある</a>
+                    <a href="https://www.google.co.jp/maps/dir/%E6%9D%B1%E4%BA%AC%E9%83%BD%E6%96%B0%E5%AE%BF%E5%8C%BA%E8%A5%BF%E6%96%B0%E5%AE%BF%EF%BC%91%E4%B8%81%E7%9B%AE%EF%BC%97+%E7%B7%8F%E5%90%88%E6%A0%A1%E8%88%8E%E3%82%B3%E3%82%AF%E3%83%BC%E3%83%B3%E3%82%BF%E3%83%AF+HAL%E6%9D%B1%E4%BA%AC/%E6%9D%B1%E4%BA%AC%E9%83%BD%E6%96%B0%E5%AE%BF%E5%8C%BA%E6%96%B0%E5%AE%BF%EF%BC%93%E4%B8%81%E7%9B%AE%EF%BC%93%EF%BC%95%E2%88%92%EF%BC%91%EF%BC%93+%E3%82%88%E3%81%A3%E3%81%A6%E3%81%93%E3%82%84+%E6%96%B0%E5%AE%BF%E5%8D%97%E5%8F%A3%E5%BA%97/@35.689994,139.6979404,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x60188cd6981dce11:0xee411b3fa3fc9f13!2m2!1d139.6969368!2d35.6916554!1m5!1m1!1s0x60188cdaf48693a1:0x4d4ff726e7fb4a72!2m2!1d139.7026238!2d35.6901278!3e0" class="btn4">GoogleMapでルート案内する</a>
 
                 </div>
+
             </div>
-    </form>
+        </form>
     </div>
 </template>
 
 <script>
     import Vue from 'vue'
     import VueHead from 'vue-head'
-
     Vue.use(VueHead)
     export default {
         name: 'WikiPage',
         data: function(){
-          return{
-              screenwidth: 375,
-              store_id: this.$route.params.id,
-              store_name: String,
-              text: String,
-              user_sum: Number,
-              phone_number: String,
-              lat: Number,
-              lng: Number
-          }
+            return{
+                screenwidth: 375,
+                store_id: this.$route.params.id,
+                store_name: String,
+                text: String,
+                user_sum: Number,
+                phone_number: String,
+                lat: Number,
+                lng: Number
+            }
         },
         created: function() {
             const url = '/wiki/detail';
             axios.post(url,{store_id:this.store_id})
-        .then(response => {
-            console.log(response.data.result[0]);
-            this.store_name = response.data.result[0].store_name;
-            this.text = response.data.result[0].text;
-            this.user_sum = response.data.result[0].store_user_sum;
-            this.phone_number = response.data.result[0].phone_number;
-            this.lat = response.data.result[0].lat;
-            this.lng = response.data.result[0].lng;
-        })
+                .then(response => {
+                    console.log(response.data.result[0]);
+                    this.store_name = response.data.result[0].store_name;
+                    this.text = response.data.result[0].text;
+                    this.user_sum = response.data.result[0].store_user_sum;
+                    this.phone_number = response.data.result[0].phone_number;
+                    this.lat = response.data.result[0].lat;
+                    this.lng = response.data.result[0].lng;
+                })
         },
         head: {
             meta: [{ name: 'viewport', content: 'width=375' }]
@@ -139,11 +136,15 @@
 
 <!--- SCSS ----------------------------------------------------------------------------------------------------------->
 <style scoped lang="scss">
-    .wrap {
+    .wraps {
+        margin-right: 0;
         width: 375px;
-        height: 1000px;
+        min-height: 1800px;
         display: flex;
         flex-direction: column;
+        //border: dashed 2px #5b8bd0;
+        //border-radius: 5px;
+        //padding: 3px;
         background-color: #2A2E43;
         justify-content: center;
         align-items: center;
@@ -174,17 +175,20 @@
             }
             .name{
                 font-family: 'MyFont';
+                /*
                 position: absolute;
                 top: 21%;
                 left: 19%;
                 transform: translateX(-25%);
+                */
+                margin: 230px 0 0 40px;
                 font-size: 30px;
-                z-index: 11;
+                z-index: 300;
                 color: white;
             }
             .yasi{
                 position: absolute;
-                top: 2%;
+                top: 0%;
                 left: 6%;
                 font-size: 20px;
                 z-index: 12;
@@ -192,9 +196,11 @@
             }
         }
         .main_line{
+            /*
             position: relative;
             top: 63%;
-            margin-top: 20px;
+            */
+            margin-top: 300px;
             width: 375px;
             height: auto;
             z-index: 40;
@@ -207,13 +213,10 @@
                 font-size: 25px;
                 color: white;
                 position: relative;
-                top: -3%;
-                //right: 0;
-                //bottom: 0;
-                //left: 10%;
+                top: 5px;
                 margin-left: 30px;
             }
-            hr{
+            .hhhh{
                 border-width: 1px 0px 0px 0px;
                 border-style: solid;
                 border-color: #cdcdcd;
@@ -236,10 +239,10 @@
                 margin-top: 20px;
                 list-style: none;
                 width: calc(100%/5);
-                padding:0 5px;
-                box-sizing:border-box;
+                padding: 0 5px;
+                box-sizing: border-box;
                 position: relative;
-                top: 50%;
+                top: 25px;
                 left: 15%;
                 img {
                     transform: translate(-50%,-50%);
@@ -259,22 +262,27 @@
             color: #A9A9A9;
             //border:solid 1px #ccc; //dbg
             p{
-                padding-left: 20px;
+                padding-left: 33px;
                 padding-right: 20px;
             }
         }
         .chart{
+            width: 100%;
+            height: 300px;
+            z-index: 500;
+            display: flex;
+            justify-content: center;
+            position: relative;
+            top: 10px;
             img {
-                position: absolute;
-                top: 86%;
-                left: 50%;
-                transform: translate(-50%,-50%);
-
+                width: 86%;
+                /*height: 100%;*/
             }
         }
 
         ////////////////////////////////////////////////////////////////////////////////////
 
+        /*
         .second_text{
             position: absolute;
             top: 100%;
@@ -282,44 +290,59 @@
             height: auto;
             background-color: #2A2E43;
             //background-color: crimson; //dbg
-            hr{
-                margin-top: 20px;
-                border-width: 1px 0px 0px 0px;
-                border-style: solid;
-                border-color: #cdcdcd;
-                margin-right: 20px;
-                margin-left: 20px;
-            }
-            .phone_g{
-                margin-top: 30px;
-                margin-bottom: 30px;
+        */
+        .hoge{
+            margin-top: 20px;
+            border-width: 1px 0px 0px 0px;
+            border-style: solid;
+            border-color: #cdcdcd;
+            margin-right: 20px;
+            margin-left: 20px;
+        }
+        .hoge2{
+            margin-top: 10px;
+        }
+
+
+
+        ///////////////////////////////////////////////////////////////////////////
+        .phone_g{
+            margin-top: 10px;
+            margin-bottom: 10px;
+            width: 100%;
+            height: 100px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            .aaaaaaaaaaaaaaa{
                 display: flex;
-                position: relative;
-                left: 100%;
-                transform: translateX(-80%);
+                align-items: center;
                 p{
-                    margin-left: 20px;
+                    margin-left: 30px;
                     color: #A9A9A9;
                 }
             }
-            iframe{
-                margin-top: 20px;
-                margin-left: 40px;
-            }
-            .Sub_Midashi{
-                font-size: 25px;
-                color: white;
-                margin-top: 20px;
-                margin-left: 30px;
-            }
-            .Buttons{
-                width: 100%;
-                display: flex;
-                flex-direction: column;
-                margin:20px 0 20px 0;
-            }
 
         }
+        iframe{
+            margin-top: 20px;
+            margin-left: 40px;
+        }
+        .Sub_Midashi{
+            font-size: 25px;
+            color: white;
+            margin-top: 20px;
+            margin-left: 30px;
+        }
+        .Buttons{
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            margin:20px 0 20px 0;
+        }
+        /////////////////////////////////////////////////////////////////////////
+
+
     }
 
 </style>
@@ -329,6 +352,7 @@
 <!--- CSS ----------------------------------------------------------------------------------------------------------->
 
 <style scoped lang="css">
+
     @font-face {
         font-family: 'MyFont';
         src: url(../Ozawa_files/NotoSerifJP-Bold.otf);
@@ -412,21 +436,21 @@
 
 
 
-.edit{
-    position: absolute;
-    left: 70%;
-    top: 33%;
-    background: #fff;
-    border: 1px solid #fff;
-    cursor: pointer;
-    border-radius: 2px;
-    color: #a18d6c;
-    font-family: 'Exo', sans-serif;
-    font-size: 14px;
-    font-weight: 400;
-}
-.edit:hover{
-    opacity: 0.8;
-}
+    .edit{
+        position: absolute;
+        left: 70%;
+        top: 322px;
+        background: #fff;
+        border: 1px solid #fff;
+        cursor: pointer;
+        border-radius: 2px;
+        color: #a18d6c;
+        font-family: 'Exo', sans-serif;
+        font-size: 14px;
+        font-weight: 400;
+    }
+    .edit:hover{
+        opacity: 0.8;
+    }
 
 </style>
