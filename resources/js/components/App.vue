7d87d77d3fe88b0f8b8fe52325b5f248803cf781
Cherry <ex00ia2432@gmail.com>
2020-07-29 14:31:16 +0900

    [clean]

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -11,14 +11,7 @@
 <script>
     export default {
         name: "App",
-        metaInfo: {
-            // if no subcomponents specify a metaInfo.title, this title will be used
-            title: 'Default Title',
-            // all titles will be injected into this template
-            // meta:[
-            //     { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-            // ]
-        }
+        metaInfo: {}
     }
 </script>
 
